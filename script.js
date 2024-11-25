6d6325a2e832f220e362dddb889d065201c50f14
6llgrushall6 <sherg6431@gmail.com>
2024-11-25 20:01:46 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 function goToHomePage() {
     window.location.href = 'index.html'; // Перенаправление на главную страницу (index.html)
 }
+
+
+// Получаем элементы
+const button = document.getElementById('showButton');
+const text = document.getElementById('hiddenText');
+
+// Обработчик клика по кнопке
+button.addEventListener('click', function() {
+    if (text.style.display === 'none') {
+        text.style.display = 'block'; // Показываем текст
+        button.textContent = 'Скрыть текст'; // Меняем текст кнопки
+    } else {
+        text.style.display = 'none'; // Скрываем текст
+        button.textContent = 'Показать текст'; // Меняем текст кнопки
+    }
+});
